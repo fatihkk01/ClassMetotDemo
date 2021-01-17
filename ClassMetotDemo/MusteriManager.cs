4b58603025e657617c876755f7091cc35f7f9c88
Fatih Kırık <fatih.kirik@bilgiedu.net>
2021-01-17 17:33:53 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void MusteriEkle(ref ArrayList musteriler,Musteri musteri)
+        {
+            musteriler.Add(musteri);
+        }
+
+        public void MusteriSil(ref ArrayList musteriler,int id)
+        {
+        
+            foreach(Musteri m in musteriler)
+            {
+                if (m.Id == id)
+                {
+                    musteriler.Remove(m);
+                    break;
+                }
+            }
+        }
+
+        public void MusteriListele(ArrayList musteriler)
+        {
+            Console.WriteLine("----------MUSTERİLER----------");
+            Console.WriteLine("------------------------------");
+            foreach (Musteri m in musteriler)
+            {
+                Console.WriteLine("Id : " + m.Id);
+                Console.WriteLine("Ad : " + m.Ad);
+                Console.WriteLine("Soyad : " + m.Soyad); 
+                Console.WriteLine("------------------------------");
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri m1 = new Musteri();
+            m1.Id = 1;
+            m1.Ad = "Hakan";
+            m1.Soyad = "Akpınar";
+
+            Musteri m2 = new Musteri();
+            m2.Id = 2;
+            m2.Ad = "Ali";
+            m2.Soyad = "Yıldırım";
+
+            Musteri m3 = new Musteri();
+            m3.Id = 3;
+            m3.Ad = "Kemal";
+            m3.Soyad = "Arslan";
+
+            Musteri m4 = new Musteri();
+            m4.Id = 4;
+            m4.Ad = "Mehmet";
+            m4.Soyad = "Poyraz";
+
+
+            ArrayList musteriler = new ArrayList();
+
+            musteriler.Add(m1);
+            musteriler.Add(m2);
+            musteriler.Add(m3);
+
+            
+            musteriManager.MusteriListele(musteriler);
+
+            musteriManager.MusteriEkle(ref musteriler, m4);
+
+            musteriManager.MusteriListele(musteriler);
+
+            musteriManager.MusteriSil(ref musteriler, 1);
+
+            musteriManager.MusteriListele(musteriler);
+
+
+
+
+
+        }
+    }
+}
